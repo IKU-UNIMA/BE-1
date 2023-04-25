32b3a2f956dc4c6707bf0365f2abce4063967d13
Adhicitta Masran <adhicittamasran@gmail.com>
2023-04-25 15:25:50 +0800

    setup validator

--- go.mod
+++ go.mod
@@ -10,6 +10,13 @@ require (
 )
 
 require (
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.2.2 // indirect
+)
+
+require (
+	github.com/go-playground/validator/v10 v10.12.0
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
--- src/api/route/route.go
+++ src/api/route/route.go
@@ -1,6 +1,9 @@
 package route
 
 import (
+	"BE-1/src/util/validation"
+
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -8,6 +11,8 @@ import (
 func InitServer() *echo.Echo {
 	app := echo.New()
 	app.Use(middleware.CORS())
+
+	app.Validator = &validation.CustomValidator{Validator: validator.New()}
 
 	app.GET("", func(c echo.Context) error {
 		return c.JSON(200, "Welcome to IKU 1 API")
