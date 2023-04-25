28bfcad79fd4b919fad690a5f9f2f9b32ec300cd
Adhicitta Masran <adhicittamasran@gmail.com>
2023-04-25 21:11:51 +0800

    umum: disable auto increment in provinsi and kab-kota id

--- src/model/kab-kota.go
+++ src/model/kab-kota.go
@@ -1,7 +1,7 @@
 package model
 
 type KabKota struct {
-	ID         int `gorm:"primaryKey"`
+	ID         int `gorm:"primaryKey;autoIncrement:false"`
 	IdProvinsi int
 	Nama       string   `gorm:"type:varchar(255)"`
 	Provinsi   Provinsi `gorm:"foreignKey:IdProvinsi"`
