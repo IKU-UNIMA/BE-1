135b32638ff9e16e3c8bf7b47bf5729a4383f8ea
Adhicitta Masran <adhicittamasran@gmail.com>
2023-05-03 17:38:38 +0800

    akun: add nik and npwp field to the register request

--- src/api/request/akun.go
+++ src/api/request/akun.go
@@ -15,18 +15,27 @@ type CheckNIM struct {
 }
 
 type RegisterAlumni struct {
-	Nim      string `json:"nim" validate:"required"`
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required"`
+	Email    string  `json:"email" validate:"required,email"`
+	Password string  `json:"password" validate:"required"`
+	Nim      string  `json:"nim" validate:"required"`
+	Nik      *int    `json:"nik"`
+	Npwp     *string `json:"npwp"`
 }
 
 type ChangePassword struct {
 	PasswordBaru string `json:"password_baru" validate:"required"`
 }
 
-func (r *RegisterAlumni) MapRequest() *model.Akun {
+func (r *RegisterAlumni) MapRequestToAkun() *model.Akun {
 	return &model.Akun{
 		Email:    r.Email,
 		Password: util.HashPassword(r.Password),
+	}
+}
+
+func (r *RegisterAlumni) MapRequestToAlumni() *model.Alumni {
+	return &model.Alumni{
+		Nik:  r.Nik,
+		Npwp: r.Npwp,
 	}
 }
